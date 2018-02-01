fc4f83e58790c6b29252c5b157190c82074fd33f
Pi <KJ4TIP@gmail.com>
2018-01-31 21:16:35 -0600

    Set up initial incoming stream processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Could not bind to socket");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Received connection");
+    }
 }
